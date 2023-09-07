7773b3648ce1c368868e7c0b2a5e10a4c103dfa0
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-09-07 23:36:11 +0530

    types of constructors

--- OOPS/constructor.java
+++ OOPS/constructor.java
@@ -2,6 +2,7 @@ public class constructor {
     public static void main(String[] args) {
         student s1 = new student("CG");
         System.out.println(s1.name);
+        student s2 = new student();
     }
     
 }
@@ -9,9 +10,11 @@ class student{
     String name;
     int rollno;
 
-    student(String name){
-        System.out.println("Constructer is called...");
+    student(String name){//parameterized constructor;
         this.name=name;
+    }
+    student(){  //non-parameterized constructor;
+        System.out.println("Constructer is called...");
     }
 
 }
