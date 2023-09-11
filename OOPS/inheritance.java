876525971ece8906977add063e631b011c9c6ea1
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-09-11 23:55:56 +0530

    inheritance basic

---
+++ OOPS/inheritance.java
@@ -0,0 +1,24 @@
+public class inheritance {
+    public static void main(String args[]){
+        Fish tuna = new Fish();
+        tuna.eat();
+    }
+}
+//Base Class
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("Eats");
+    }
+    void breathe(){
+        System.out.println("Breathes");
+    }
+}
+
+//Derived Class
+class Fish extends Animal{
+    int fins;
+    void swim(){
+        System.out.println("Swims in water");
+    }
+}
