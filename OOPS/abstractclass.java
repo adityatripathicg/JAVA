06c7b8dd10cc4eb243ae614058ee8bfb5e5114df
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-09-13 20:27:25 +0530

    abstractclass

---
+++ OOPS/abstractclass.java
@@ -0,0 +1,30 @@
+public class abstractclass {
+    public static void main(String args[]){
+    horse h1 = new horse();
+    h1.eat();
+    h1.walk();
+    System.out.println(h1.Color);
+    h1.changeColor();
+    System.out.println(h1.Color);
+    }
+    //Animal a = new Animal();{//object of abstract class can't be created 
+}
+
+abstract class Animal{//object of abstract class can't be created 
+    void eat(){
+        System.out.println("Animal eats");
+    }
+    String Color;
+    Animal(){//constructor
+        Color = "brown";
+    }
+    abstract void walk();//abstract function can only be declared
+}
+class horse extends Animal{
+    void changeColor(){
+        Color = "dark brown";
+    }
+    void walk(){//it is mandatory to define abstract function in every derived class.
+        System.out.println("walks on 4 legs");//
+    }
+}
