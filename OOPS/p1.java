fa3121e5c00fc4f97f756460c2ef3887b9ce8577
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-09-14 07:01:23 +0530

    random

---
+++ OOPS/p1.java
@@ -0,0 +1,10 @@
+class Student{
+    String name;
+}
+public class p1 {
+    public static void main(String args[]){
+        Student s = new Student(); 
+        s.name = "cg";
+        System.out.println(s.name);
+    }
+}
